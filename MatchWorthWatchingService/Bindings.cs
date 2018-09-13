4207d7737f1b6bbb5528328f1ad4e0cff0691319
Jeremy Bean <jeremy.david.bean@gmail.com>
2018-09-13 12:41:05 -0600

    Adding Ninject

---
+++ MatchWorthWatchingService/Bindings.cs
@@ -0,0 +1,26 @@
+﻿using MatchesWorthWatching.Common;
+using MatchInterestCalculator;
+using MatchWorthWatchingService.DAL;
+using MatchWorthWatchingService.FootballAPI;
+using MatchWorthWatchingService.Logging;
+using MatchWorthWatchingService.Twitter;
+using Ninject.Modules;
+
+namespace MatchWorthWatchingService
+{
+    public class Bindings : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ILogger>().To<ConsoleLogger>();
+            Bind<IFootballAPIClient>().To<FootballAPIClient>();
+            Bind<ITweetBuilder>().To<TweetBuilder>();
+            Bind<IDatabaseOperations>().To<DatabaseOperations>();
+            Bind<IInterestCalculator>().To<InterestCalculator>();
+
+            //TODO: when Twitter connection is done, use the real Twitter service
+            //Bind<ITwitterService>().To<TwitterService>();
+            Bind<ITwitterService>().To<LocalLoggingTwitterService>();
+        }
+    }
+}
--- MatchWorthWatchingService/Workflow.cs
+++ MatchWorthWatchingService/Workflow.cs
@@ -7,9 +7,6 @@ using MatchWorthWatchingService.WorkflowSteps;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MatchInterestCalculator;
 using MatchesWorthWatching.Common;
 
 namespace MatchWorthWatchingService
